f804e1ed15feea666fa66877ae403f5fb44fb3f3
gowhale <gabewhale@gmail.com>
2020-10-10 13:48:26 +0100

    Intial structure of program created

---
+++ main.py
@@ -0,0 +1,19 @@
+# Author: gowhale
+# Date: 10/10/2020
+# This file has functions which generate questions for a scattegory type of game
+# I am following the PEP 8 Python coding standards: https://www.python.org/dev/peps/pep-0008/
+
+
+# This function will pick a random letter between A-Z
+def generate_random_letter():
+    return ""
+
+
+# This function will select questions from a text file
+def select_questions(n):
+    return []
+
+
+# This function will print out the letter selected and the questions to answer
+def display_task(questions):
+    print()
